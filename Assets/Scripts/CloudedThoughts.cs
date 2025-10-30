6981b35eac1070b3f9819507496083fe091de948
OmkaarRege <omkaarrege1@gmail.com>
2025-10-30 14:58:08 -0400

    more stuff

--- Assets/Scripts/CloudedThoughts.cs
+++ Assets/Scripts/CloudedThoughts.cs
@@ -5,7 +5,7 @@ public class CloudedThoughts : MonoBehaviour
     public GameObject playerObject;
 
     private Camera mainCamera;
-    public float moveSpeed, initialX,initialY, screenWidth, screenHeight;
+    public float moveSpeed, upspeed, initialX,initialY, screenWidth, screenHeight;
 
     private Vector2 screenBounds;
     private SpriteRenderer spriteRenderer;
@@ -13,35 +13,25 @@ public class CloudedThoughts : MonoBehaviour
     void Start()
     {
         mainCamera = Camera.main;
-
         screenHeight = mainCamera.orthographicSize * 2;
         screenWidth = screenHeight * mainCamera.aspect;
-        
         SpriteRenderer spriteRenderer = playerObject.GetComponent<SpriteRenderer>();
-        
-        Debug.Log("Screen bounds X coordinate: " + screenWidth.ToString());
-    
+        initialX = playerObject.transform.position.x;
+       
     }
 
     void Update()
     {
-        if (playerObject.transform.position.x < (initialX + (screenWidth / 2)))
-        {
-
-            Vector2 movement = Vector2.zero;
-
-
-            // Check for 'W' or Up Arrow key press
-            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-            {
-                movement.y += 1f;
-            }
-
-            // Check for 'S' or Down Arrow key press
-            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-            {
-                movement.y -= 1f;
-            }
+        Move();
+        
+        Vector3 playertransform = playerObject.transform.position;
+        playertransform.x = Mathf.Clamp(playertransform.x, initialX - (screenWidth/2), initialX + (screenWidth/2));
+        playerObject.transform.position = playertransform;
+    }
+    
+    void Move()
+    {
+        Vector2 movement = new Vector2(0,upspeed);
 
             // Check for 'A' or Left Arrow key press
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
@@ -64,9 +54,9 @@ public class CloudedThoughts : MonoBehaviour
             // Apply movement to the object's position
             playerObject.transform.Translate(movement * moveSpeed * Time.deltaTime);
         }
-            
-       
-    }
+
+
+    
 
        
 
